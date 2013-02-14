dbb57cacb1cb625054835ba9fce11382e778547c
Kareem Grant <kareem@getuserwise.com>
2013-02-14 08:51:38 -0700

    completed scrabble warmup

---
+++ 03-scrabble/scrabble.rb
@@ -0,0 +1,30 @@
+class Scrabble
+
+  VALUES = {
+      1 => %w(a e i o u l n r s t),
+      2 => %w(d g),
+      3 => %w(b c m p),
+      4 => %w(f h v w y),
+      5 => %w(k),
+      8 => %w(j x),
+      10 => %w(q z),
+    }
+
+  def self.score(word)
+    letters = word.split("")
+    scores = letters.collect{|letter| lookup(letter)}
+
+    #puts scores.inspect
+    scores.inject(0){|sum, value| sum + value}
+  end
+
+  def self.lookup(letter)
+    VALUES.each do |key, values|
+      return key if values.include?(letter)
+    end
+  end
+
+
+end
+
+puts Scrabble.score("kareem")
